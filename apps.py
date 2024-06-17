e7eab8c5bfc2f6937f3b1efbb4eae359dcc97366
Wanderer22011 <22011@burnside.school.nz>
2024-06-18 09:24:59 +1200

    Finding IGN Rating revamp, you can now type your own number!

--- apps.py
+++ apps.py
@@ -4,8 +4,11 @@ Made by Wilson Tong on the 22/05/24'''
 # Gains acess to Sql codes and data
 import sqlite3
 
+
+
 # Variable Declarations:
 DATABASE="Game_Database.db"
+conn = None
 
 #Functions To Look At All The Games
 def Print_All_Games():
@@ -520,43 +523,10 @@ def Print_All_Data_Only_Year_2023():
     db.close
 
 #Functions to pick the specific IGN rating
-def Print_All_Data_Only_IGN_Rating_7():
+def Print_All_Data_Only_IGN_Rating(name):
     db=sqlite3.connect(DATABASE)
     cursor=db.cursor()
-    sql="SELECT Games.game_name, Genres.genre_name, Makers.maker_name, Games.IGN_rating, Games.release_year FROM Games JOIN Genres ON Genres.genre_id = Games.genre_id JOIN Makers ON Makers.maker_id=Games.maker_id WHERE Games.IGN_rating='7';"
-    cursor.execute(sql)
-    results=cursor.fetchall()
-    print("Game_Name                                         Genre                                   Maker                    IGN_Rating           Year_Released\n")
-    for final in results:
-        print(f"{final[0]:<50}{final[1]:<40}{final[2]:<30}{final[3]:<20}{final[4]}")
-    db.close
-
-def Print_All_Data_Only_IGN_Rating_8():
-    db=sqlite3.connect(DATABASE)
-    cursor=db.cursor()
-    sql="SELECT Games.game_name, Genres.genre_name, Makers.maker_name, Games.IGN_rating, Games.release_year FROM Games JOIN Genres ON Genres.genre_id = Games.genre_id JOIN Makers ON Makers.maker_id=Games.maker_id WHERE Games.IGN_rating='8';"
-    cursor.execute(sql)
-    results=cursor.fetchall()
-    print("Game_Name                                         Genre                                   Maker                    IGN_Rating           Year_Released\n")
-    for final in results:
-        print(f"{final[0]:<50}{final[1]:<40}{final[2]:<30}{final[3]:<20}{final[4]}")
-    db.close
-
-def Print_All_Data_Only_IGN_Rating_9():
-    db=sqlite3.connect(DATABASE)
-    cursor=db.cursor()
-    sql="SELECT Games.game_name, Genres.genre_name, Makers.maker_name, Games.IGN_rating, Games.release_year FROM Games JOIN Genres ON Genres.genre_id = Games.genre_id JOIN Makers ON Makers.maker_id=Games.maker_id WHERE Games.IGN_rating='9';"
-    cursor.execute(sql)
-    results=cursor.fetchall()
-    print("Game_Name                                         Genre                                   Maker                    IGN_Rating           Year_Released\n")
-    for final in results:
-        print(f"{final[0]:<50}{final[1]:<40}{final[2]:<30}{final[3]:<20}{final[4]}")
-    db.close
-
-def Print_All_Data_Only_IGN_Rating_10():
-    db=sqlite3.connect(DATABASE)
-    cursor=db.cursor()
-    sql="SELECT Games.game_name, Genres.genre_name, Makers.maker_name, Games.IGN_rating, Games.release_year FROM Games JOIN Genres ON Genres.genre_id = Games.genre_id JOIN Makers ON Makers.maker_id=Games.maker_id WHERE Games.IGN_rating='10';"
+    sql="SELECT Games.game_name, Genres.genre_name, Makers.maker_name, Games.IGN_rating, Games.release_year FROM Games JOIN Genres ON Genres.genre_id = Games.genre_id JOIN Makers ON Makers.maker_id=Games.maker_id WHERE Games.IGN_rating="+name+";"
     cursor.execute(sql)
     results=cursor.fetchall()
     print("Game_Name                                         Genre                                   Maker                    IGN_Rating           Year_Released\n")
@@ -569,17 +539,18 @@ def Print_All_Data_Only_IGN_Rating_10():
 while True:
     #Asks what the user wants to do
     user_input=input(
-    """
+"""
 
-       Hello User, What Would You Like To Do Today In This Game Database?
-       Please Enter A Number From The Options Bellow:
+Hello User, What Would You Like To Do Today In This Game Database?
+Please Enter A Number From The Options Bellow:
 
-       Enter '1' To Look At All The Games In This Database
-       Enter '2' To Check Information About The Data
-       Enter '3' To Find Data
-       Enter '4' To Order Data
-       Enter '5' To Exit This Program
-       """)
+Enter '1' To Look At All The Games In This Database
+Enter '2' To Check Information About The Data
+Enter '3' To Find Data
+Enter '4' To Order Data
+Enter '5' To Exit This Program
+       
+""")
     #prints the data the user wants
     if user_input=="1":
         Print_All_Games()
@@ -587,13 +558,13 @@ while True:
     
     elif user_input=="2":
         user_input2=input(
-    """
-        Which Information would u like to see?
-        Enter '1' To See All of The Information 
-        Enter '2' To See All of The Maker Information
-        Enter '3' To See All of The Genre Information
-        Enter '4' To Exit This Page
-        Enter '5' To Exit This Program
+"""
+Which Information would u like to see?
+Enter '1' To See All of The Information 
+Enter '2' To See All of The Maker Information
+Enter '3' To See All of The Genre Information
+Enter '4' To Exit This Page
+Enter '5' To Exit This Program
             """)
         if user_input2=="1":
             Print_All_Data()
@@ -622,34 +593,34 @@ while True:
     
     elif user_input=="3":
         user_input3=input(
-        '''
-        What Would You Like To Find?
-        Enter '1' To Find All The Games With The Specific Genre
-        Enter '2' To Find All The Games With The Specific Maker
-        Enter '3' To Find All The Games With The Specific Release Year
-        Enter '4' To Find All The Games With The Specific IGN Rating
-        Enter '5' To Exit This Page
-        Enter '6' To Exit This Program
-        '''
+'''
+What Would You Like To Find?
+Enter '1' To Find All The Games With The Specific Genre
+Enter '2' To Find All The Games With The Specific Maker
+Enter '3' To Find All The Games With The Specific Release Year
+Enter '4' To Find All The Games With The Specific IGN Rating
+Enter '5' To Exit This Page
+Enter '6' To Exit This Program
+'''
         )
         if user_input3=="1":
             user_input3A=input(
-            """ 
-            What Genre Would You Like To See?
-            Enter '1' To Look At Games That Have 'Sandbox' Genre
-            Enter '2' To Look At Games That Have 'Battle Royale' Genre
-            Enter '3' To Look At Games That Have 'MOBA' Genre
-            Enter '4' To Look At Games That Have 'FPS' Genre
-            Enter '5' To Look At Games That Have 'Sports' Genre
-            Enter '6' To Look At Games That Have 'Fighting' Genre
-            Enter '7' To Look At Games That Have 'RPG' Genre
-            Enter '8' To Look At Games That Have 'Social Simulation' Genre
-            Enter '9' To Look At Games That Have 'Action-Adventure' Genre
-            Enter '10' To Look At Games That Have 'Party Game' Genre
-            Enter '11' To Exit This Page
-            Enter '12' To Exit This Program
+""" 
+What Genre Would You Like To See?
+Enter '1' To Look At Games That Have 'Sandbox' Genre
+Enter '2' To Look At Games That Have 'Battle Royale' Genre
+Enter '3' To Look At Games That Have 'MOBA' Genre
+Enter '4' To Look At Games That Have 'FPS' Genre
+Enter '5' To Look At Games That Have 'Sports' Genre
+Enter '6' To Look At Games That Have 'Fighting' Genre
+Enter '7' To Look At Games That Have 'RPG' Genre
+Enter '8' To Look At Games That Have 'Social Simulation' Genre
+Enter '9' To Look At Games That Have 'Action-Adventure' Genre
+Enter '10' To Look At Games That Have 'Party Game' Genre
+Enter '11' To Exit This Page
+Enter '12' To Exit This Program
 
-            """
+"""
             )
             if user_input3A=="1":
                 print("\n")
@@ -704,28 +675,28 @@ while True:
         
         elif user_input3=="2":
             user_input3B= input(
-        '''
-        Which Maker Would You Like To See?
-        Enter '1' To Find Game/s Made By 'Mojang Studios' In This Database
-        Enter '2' To Find Game/s Made By 'Riot Games' In This Database
-        Enter '3' To Find Game/s Made By 'Psyonix' In This Database
-        Enter '4' To Find Game/s Made By 'Supercell' In This Database
-        Enter '5' To Find Game/s Made By 'Bandai Namco Studios' In This Database
-        Enter '6' To Find Game/s Made By 'Valve Corporation' In This Database
-        Enter '7' To Find Game/s Made By 'Nintendo EPD' In This Database
-        Enter '8' To Find Game/s Made By 'miHoYo' In This Database
-        Enter '9' To Find Game/s Made By 'FromSoftware' In This Database
-        Enter '10' To Find Game/s Made By 'The Sims Studio' In This Database
-        Enter '11' To Find Game/s Made By 'Rockstar Games' In This Database
-        Enter '12' To Find Game/s Made By 'Blizzard Entertainment' In This Database
-        Enter '13' To Find Game/s Made By 'Respawn Entertainment' In This Database
-        Enter '14' To Find Game/s Made By 'CD PROJEKT RED' In This Database
-        Enter '15' To Find Game/s Made By 'InnerSlothLLC' In This Database
-        Enter '16' To Find Game/s Made By 'Epic Games' In This Database
-        Enter '17' To Exit This Page
-        Enter '18' To Exit This Program
+'''
+Which Maker Would You Like To See?
+Enter '1' To Find Game/s Made By 'Mojang Studios' In This Database
+Enter '2' To Find Game/s Made By 'Riot Games' In This Database
+Enter '3' To Find Game/s Made By 'Psyonix' In This Database
+Enter '4' To Find Game/s Made By 'Supercell' In This Database
+Enter '5' To Find Game/s Made By 'Bandai Namco Studios' In This Database
+Enter '6' To Find Game/s Made By 'Valve Corporation' In This Database
+Enter '7' To Find Game/s Made By 'Nintendo EPD' In This Database
+Enter '8' To Find Game/s Made By 'miHoYo' In This Database
+Enter '9' To Find Game/s Made By 'FromSoftware' In This Database
+Enter '10' To Find Game/s Made By 'The Sims Studio' In This Database
+Enter '11' To Find Game/s Made By 'Rockstar Games' In This Database
+Enter '12' To Find Game/s Made By 'Blizzard Entertainment' In This Database
+Enter '13' To Find Game/s Made By 'Respawn Entertainment' In This Database
+Enter '14' To Find Game/s Made By 'CD PROJEKT RED' In This Database
+Enter '15' To Find Game/s Made By 'InnerSlothLLC' In This Database
+Enter '16' To Find Game/s Made By 'Epic Games' In This Database
+Enter '17' To Exit This Page
+Enter '18' To Exit This Program
 
-        ''')
+''')
             if user_input3B=="1":
                 print("\n")
                 Print_All_Data_Only_Maker_Mojang_Studios()
@@ -805,23 +776,23 @@ while True:
 
         elif user_input3=="3":
             user_input3C= input(
-            '''
-            Which Year Would You Like To See?
-            Enter '1' To Find All The Game/s Released In 2009
-            Enter '2' To Find All The Game/s Released In 2011
-            Enter '3' To Find All The Game/s Released In 2013
-            Enter '4' To Find All The Game/s Released In 2014
-            Enter '5' To Find All The Game/s Released In 2015
-            Enter '6' To Find All The Game/s Released In 2017
-            Enter '7' To Find All The Game/s Released In 2018
-            Enter '8' To Find All The Game/s Released In 2019
-            Enter '9' To Find All The Game/s Released In 2020
-            Enter '10' To Find All The Game/s Released In 2022
-            Enter '11' To Find All The Game/s Released In 2023
-            Enter '12' To Exit This Page
-            Enter '13' To Exit This Program
+'''
+Which Year Would You Like To See?
+Enter '1' To Find All The Game/s Released In 2009
+Enter '2' To Find All The Game/s Released In 2011
+Enter '3' To Find All The Game/s Released In 2013
+Enter '4' To Find All The Game/s Released In 2014
+Enter '5' To Find All The Game/s Released In 2015
+Enter '6' To Find All The Game/s Released In 2017
+Enter '7' To Find All The Game/s Released In 2018
+Enter '8' To Find All The Game/s Released In 2019
+Enter '9' To Find All The Game/s Released In 2020
+Enter '10' To Find All The Game/s Released In 2022
+Enter '11' To Find All The Game/s Released In 2023
+Enter '12' To Exit This Page
+Enter '13' To Exit This Program
 
-    	    '''
+'''
             )
             if user_input3C=="1":
                 print("\n")
@@ -880,42 +851,20 @@ while True:
 
         elif user_input3=="4":
             user_input3D= input(
-        '''
-        Which IGN Rating Would You Like To See?
-        Enter '1' To Find All The Games With IGN Rating Of '7'
-        Enter '2' To Find All The Games With IGN Rating Of '8'
-        Enter '3' To Find All The Games With IGN Rating Of '9'
-        Enter '4' To Find All The Games With IGN Rating Of '10'
-        Enter '5' To Exit This Page
-        Enter '6' To Exit This Program
-        '''
-        )
-            if user_input3D=='1':
-                print("\n")
-                Print_All_Data_Only_IGN_Rating_7()
-            
-            elif user_input3D=='2':
-                print("\n")
-                Print_All_Data_Only_IGN_Rating_8()
-            
-            elif user_input3D=='3':
-                print("\n")
-                Print_All_Data_Only_IGN_Rating_9()
-            
-            elif user_input3D=='4':
-                print("\n")
-                Print_All_Data_Only_IGN_Rating_10()
-            
-            elif user_input3D=="5":
-                continue
+'''
+Which IGN Rating Would You Like To See?
+Type the number you wanted to check from 1-10:
 
-            elif user_input3D=="6":
-                print("Ok, See You Next Time.")
-                break
-
+'''
+    )
+            if user_input3D >="7":
+                if user_input3D <="10":
+                    Print_All_Data_Only_IGN_Rating(user_input3D)
+                
+                else:
+                    print("No data found for the number:", user_input3D)
             else:
-                print("This is not an option, please try again.\n")
-                continue
+                print("No data found for the number:", user_input3D)
 
 
         elif user_input3=="5":
@@ -931,17 +880,17 @@ while True:
 
     elif user_input=="4":
         user_input4= input(
-    """ 
-        Which Order Would You Like To Sort The Data?
-        Enter '1' To Order the Data By Game Name
-        Enter '2' To Order The Data By Genre Name
-        Enter '3' To Order The Data By Maker Name
-        Enter '4' To Order The Data By Release Year
-        Enter '5' To Order The Data By IGN Rating
-        Enter '6' To Exit This Page
-        Enter '7' To Exit This Program
+""" 
+Which Order Would You Like To Sort The Data?
+Enter '1' To Order the Data By Game Name
+Enter '2' To Order The Data By Genre Name
+Enter '3' To Order The Data By Maker Name
+Enter '4' To Order The Data By Release Year
+Enter '5' To Order The Data By IGN Rating
+Enter '6' To Exit This Page
+Enter '7' To Exit This Program
             
-    """)
+""")
         if user_input4=="1":
             Print_All_Data_Sorted_by_Game_Name()
             print("\n")
@@ -975,3 +924,4 @@ while True:
     else:
             print("This is not an option, please try again.\n")
             continue
+    
